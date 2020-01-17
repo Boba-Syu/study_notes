5ebb806786d2b36ae4d5c70cefefe8f50d8cf313
Boba-Syu <bobasyu@163.com>
2020-01-17 08:31:46 +0800

    Scala的trait和线程学习

--- src/main/java/cn/bobasyu/scala/ThreadTest.scala
+++ src/main/java/cn/bobasyu/scala/ThreadTest.scala
@@ -1,21 +1,20 @@
 package cn.bobasyu.scala
 
-import java.util.stream.IntStream
-
 /**
  * Scala线程学习
  */
 object ThreadTest {
   def main(args: Array[String]): Unit = {
-    IntStream.range(0, 10).forEach(_ => new ThreadTest().start())
+    0 to 10 foreach { _ => new ThreadTest().start() }
   }
 }
 
 class ThreadTest extends Thread {
   override def run(): Unit = {
-    IntStream.range(0, 10).forEach(i => {
+    0 to 10 foreach { i => {
       println(s"$getName is running: $i")
       Thread.sleep(2)
-    })
+    }
+    }
   }
 }
--- src/main/java/cn/bobasyu/scala/TraitTest.scala
+++ src/main/java/cn/bobasyu/scala/TraitTest.scala
@@ -5,14 +5,25 @@ package cn.bobasyu.scala
  * trait(特征)相当于Java中的接口
  */
 object TraitTest {
+  def main(args: Array[String]): Unit = {
+    val traitTest: TraitTest = new TraitTest with myTrait {
+      override def fun1(): Unit = println("动态继承trait.")
+    }
+    traitTest.fun1()
+  }
+
+  class TraitTest {
+    def fun1(): Unit = ???
+
+  }
 
   trait myTrait {
-    def fun1()
+    def fun1(): Unit
   }
 
   class myTraitImpl extends myTrait {
     override def fun1(): Unit = {
-      println("实现trait中的抽象方法")
+      println("trait中的抽象方法.")
     }
   }
 
